a9ef5b4da91ccff78d952460a3f95b4e4a5ba177
pogacha <89231282@student.upr.si>
2024-09-10 01:13:16 +0200

    koncal day 1

--- day1/part2.R
+++ day1/part2.R
@@ -1,12 +1,9 @@
 library(tidyverse)
 
-# funkcije
-source("numFuncs.R")
-
 # dobimo podatke
-file <- read.delim("test.txt", header=F) %>% 
+file <- read.delim("data.txt", header=F) %>% 
 	unlist() %>% 
-	setNames(NULL); file
+	setNames(NULL)
 
 # moram naredit grep za vsa števila od 1 do 9 (črkovno)
 # najprej moram najt prvo in zadnjo številko
@@ -15,9 +12,6 @@ file <- read.delim("test.txt", header=F) %>%
 # dvojni for loop, ki gradi besede
 
 # max length besede je 5, right
-map(stevilaAlpha, nchar) %>% unlist() %>% max()
-# yep
-
 # vzamemo i in gledamo 4 crke naprej
 # vsak j gledamo če je alpha ali digit stevilo
 # če je se shrani v nums[1]
@@ -25,60 +19,70 @@ map(stevilaAlpha, nchar) %>% unlist() %>% max()
 # če po koncu ni drugega števila, vzamemo dvakrat prvo
 
 # spet moramo sešet vse vrednosti
-calibrationData <- 0
 
-# primer: "two1nine"
-test <- file[2] %>% str_split_1(""); test
-nums <- c(0,0)
+getCalibrationData <- \(line) {
+	nums <- c(0,0)
+	stevila <- c( one=1, two=2, three=3, four=4, five=5, six=6, seven=7, eight=8, nine=9)
 
-for (i in 1:length(test)) {
-	# hrani besedo, max len 5
-	# vsak nov i, se ponastavi
-	wordNum <- rep("", 5)
+	for (i in 1:length(line)) {
+		print(nums)
 
-    for (j in i:min(length(test), i + 4)) {
-		# 1:1, 1:2, 1:3...
-		# 2:2, 2:3, 2:4...
-		# da lahko nadaljujemo po nizu
-		print(paste("i:",i," ","j:",j))
-		if (i <= j) {
-			wordNum[j] <- test[j]
-			# gledamo, če nastane število iz črk
-			word <- paste0(wordNum, collapse="")
-			print(word)
+		for (j in 1:length(line)) {
+			# gledamo, če je prvi znak v trenutni besedi število
+			if (i == j) {
+				znak <- as.numeric(line[i:j])
 
-			if (word %in% names(stevila)) {
-				print("JE V STEVILIH")
-				# če je prvi element prost, shranimo prvo število
-				# drugače se vsako naslednje shrani v drugo število
-				if (nums[1] == 0) {
-					nums[1] <- stevila[word]
-				} else {
-					nums[2] <- stevila[word]
+				if (znak %in% stevila) {
+					# če je prvi element prost, shranimo prvo število
+					# drugače se vsako naslednje shrani v drugo število
+					if (nums[1] == 0) {
+						nums[1] <- znak
+					} else {
+						nums[2] <- znak
+					}
+				}
+			}
+			if (i <= j) {
+				word <- paste0(line[i:j], collapse="")
+
+				# gledamo, če nastane število iz črk
+				if (word %in% names(stevila)) {
+					znak <- stevila[word]
+
+					if (nums[1] == 0) {
+						nums[1] <- znak
+					} else {
+						nums[2] <- znak
+					}
 				}
 			}
 		}
-		# gledamo, če je prvi znak v trenutni besedi število
-		if (i == j) {
-			znak <- test[i:j]
+	}
 
-			if (as.numeric(znak) %in% stevila) {
-				print("JE V STEVILIH")
-				# če je prvi element prost, shranimo prvo število
-				# drugače se vsako naslednje shrani v drugo število
-			    if (nums[1] == 0) {
-					nums[1] <- as.numeric(znak)
-				} else {
-					nums[2] <- as.numeric(znak)
-				}
-			}
-			
-		}
-    }
+	# če je našlo samo eno število, moramo narediti dvomestno iz enega
+	if (nums[2] == 0) {
+		nums[2] <- nums[1]
+	}
+
+	return(as.numeric(paste0(nums, collapse="")))
 }
-nums
 
-# dodamo število v vektor
-calibrationData + as.numeric(paste0(nums, collapse=""))
+t <- str_split_1("eightwo1three", ""); t
+t <- str_split_1("8enadvatri", ""); t
+getCalibrationData(t) -> calibrationData; calibrationData
 
-paste0("", "", "e","n","a", collapse="")
+# zdaj moramo to narediti za vse vrstice
+sapply(file, str_split, pattern="") -> splited; splited
+
+# LONG PROCESS
+# imagine, da bi mel 100000 vrstic...
+map(splited, getCalibrationData) -> data; data
+
+# :D
+# še seštejemo vse vrednosti v listu
+sum(unlist(data)) -> CALIBRATION_DATA; CALIBRATION_DATA
+
+write.table(CALIBRATION_DATA, "odgovor2.txt", row.names=F, col.names=F)
+
+
+# yeah, vzame veliko časa, ampak končno dela
---
+++ day1/test.R
@@ -0,0 +1,10 @@
+library(tidyverse)
+source("numFuncs.R")
+stevila
+
+st <- c("one|two|three|four|five|six|seven|eigth|nine|ten")
+test <- c("twoNANANA")
+str_extract(string=test, pattern=st)
+
+
+
