c8b61ce4bc595e1b4927b24db7c7f7c0f7cd716a
pogacha <89231282@student.upr.si>
2024-09-25 11:47:14 +0200

    neznam day 5 for the life of me

--- day5/part2.R
+++ day5/part2.R
@@ -1,18 +1,10 @@
 library(tidyverse)
 source("C:/Users/joene/Documents/progAAAAAAA/adventOfCode2023/util.R", chdir = TRUE, echo = TRUE)
+source("fileFun.R", chdir = TRUE, echo = TRUE)
 
 # file <- readFile("testData.txt")
 file <- readFile("data.txt")
 file <- readFile("testData.txt")
-
-# dobimo data frame za vsak map
-parseData <- \(file) {
-    # funkcije so v "util.R"
-    dobiMaps(file) -> maps
-    dobiMapVrednosti(maps, file) -> maps_values
-    dobiDataFrames(maps_values) -> dfs
-    return(dfs)
-}
 
 dfs <- parseData(file)
 dfs
@@ -24,84 +16,42 @@ seeds <- file[1] %>%
     as.numeric()
 seeds
 
-# imamo 20 števil
-# liha je start, soda je len intervala semen
+seme <- 130000:1500000000
+ab <- 1:1000000000
+xy <- 10:15
+# 2 line
+validSeme <- seme < 15 & seme >= 3
+length(which(validSeme == T))
+# 1 line
+which(ab %in% seme)
+indeksi <- c(which(seme >= ab[1]), which(seme < ab[length(ab)]))
+indeksi <- seme >= ab[1] && seme < ab[length(ab)]
+indeksi
 
-# namesto enega števila, preslikamo range števil
-# moramo ugotoviti, katera semena so znotraj source intervala
+seme >= ab[1] -> more
+seme < ab[length(ab)] -> less
+more == less
 
-# seedRange je nek interval seeds
-# preslikali bomo cel range namesto posameznih števil
-
-# hočem, da vzamemo en seed range in ga modificiramo
-# in nato naslednji seed range, itd.
-dobiNoveVrednosti <- \(df, seedRange) {
-    newSeeds <- c() # vektor, ki bo nosil modificirane vrednosti
-
-    # BUG: nimam pojma, če lahko dvakrat modificiramo isto število
-    # če JA, potem moramo keepat track tistih, ki smo jih že dali skozi...
-
-    # dobimo nove vrednosti ali vrnemo seed
-    # gremo čez vrstice od posamezne map
-    for (j in 1:nrow(df)) {
-        dst_start <- df$dst[j] %>% as.numeric()
-        src_start <- df$src[j] %>% as.numeric()
-        range <- df$range[j] %>% as.numeric()
-
-        # dobimo source range, ki predstavlja valid seeds
-        src_range <- seq(src_start, src_start + range - 1)
-
-        # semena, ki so valid spremenimo
-        # ostala pustimo taka kot so
-        ids <- which(seedRange %in% src_range)
-
-        # če ni noben seed vredu, vrnemo preden modificiramo
-        if (length(ids) == 0) {
-            newSeeds <- c(newSeeds, seedRange)
-            next
-        }
-        # na ids pokličemo funkcijo, ki preslika vrednosti
-        preslikava <- \(x) {
-            return(dst_start + (x - src_start))
-        }
-        newSeeds <- c(newSeeds, unlist(sapply(seedRange[ids], preslikava)))
+for (i in 1:length(seme)) {
+    if (low == -1 && seme[i] >= ab[1]) {
+        print(paste("low is", seme[i]))
+        low <- seme[i]
     }
-    return(newSeeds)
+    if (!(seme[i] < ab[length(ab)])) {
+        print(paste("seme", seme[i], "breaks the loop"))
+        print(paste("hi is", seme[i-1]))
+        hi <- seme[i-1]
+        break
+    }
 }
 
-# od zgornje funkcije dobimo modificirane vrednosti za vsa semena
-# ampak samo za en df
-# zato moramo poslati vrednosti v pipe skozi vsak df
-# dokler ne dobimo location vrednosti
+fg <- 1:10
+fg > 5 -> en
+fg < 9 -> dv
+en == dv
 
-# pač, mi pretvorimo vse range naenkrat in vrnemo vektor vseh
-# uporabimo dani df, zato moramo zdaj pošiljati modificirane vrednosti v vsak df
-dobiLocationValues <- \(dfs, seeds) {
-    # vektor bo hrnil location values
-    location_values <- c()
-
-    for (i in seq(1, length(seeds), 2)) {
-        seedRange <- seq(seeds[i], seeds[i] + seeds[(i + 1)] - 1)
-
-        dobiNoveVrednosti(dfs[[1]], seedRange) %>%
-            dobiNoveVrednosti(dfs[[2]], .) %>%
-            dobiNoveVrednosti(dfs[[3]], .) %>%
-            dobiNoveVrednosti(dfs[[4]], .) %>%
-            dobiNoveVrednosti(dfs[[5]], .) %>%
-            dobiNoveVrednosti(dfs[[6]], .) %>%
-            dobiNoveVrednosti(dfs[[7]], .) -> newVals
-
-        location_values <- c(location_values, newVals)
-    }
-
-    return(location_values)
-}
-
-# dobimo vrednosti
-dobiLocationValues(dfs, seeds) -> LOCATIONS
-LOCATIONS
-min(LOCATIONS)
-
-# vrnemo najmanjšo
-writeText(min(LOCATIONS), "odgovor2test.txt")
-# writeText(min(LOCATIONS), "odgovor1.txt")
+seeds[ab[1]:ab[length(ab)]] -> indeksi
+ab[1]
+ab[length(ab)]
+indeksi
+xy[indeksi]
